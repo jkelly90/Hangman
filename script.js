0b60246a8d9698e0fbdf1984f4ed98e5b9858fff
jkelly90 <jlfkelly@gmail.com>
2020-02-14 11:46:16 +0100

    updated words

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const finalMessage = document.getElementById('final-message');
 
 const figureParts = document.querySelectorAll('.figure-part');
 
-const words = ['application', 'programming', 'interface', 'wizard'];
+const words = ['application', 'programming', 'javascript', 'development'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)]
 
